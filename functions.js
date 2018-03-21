aa38d68ce40acd71e993c3feeda2eeb30c6e4ac0
zamfiralina <alina.zamfir_1997@yahoo.com>
2018-03-21 23:39:36 +0200

    Javascript function added.

---
+++ functions.js
@@ -0,0 +1,9 @@
+function gotoPage(page) {
+    //document.getElementById("elementID").onclick.apply(location.href, location.href = page.toString().concat(".html"));
+
+    window.alert("S-a apelat macar :)");
+
+    //document.getElementById("testID").onclick.apply(location.href, location.href = page.toString().concat(".html"));
+
+    window.location.href = page.toString().concat(".html");
+}
