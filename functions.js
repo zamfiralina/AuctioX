1892de90c51c3fdc1fcdecc2c3789b6846e6499e
zamfiralina <alina.zamfir_1997@yahoo.com>
2018-03-22 00:17:24 +0200

    Minor changes.

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 function gotoPage(page) {
     //document.getElementById("elementID").onclick.apply(location.href, location.href = page.toString().concat(".html"));
 
-    window.alert("S-a apelat macar :)");
+    //window.alert("S-a apelat macar :)");
 
     //document.getElementById("testID").onclick.apply(location.href, location.href = page.toString().concat(".html"));
 
