32d0499a045baae4d981a27d482c8f149950a573
zamfiralina <alina.zamfir_1997@yahoo.com>
2018-05-19 13:20:45 +0300

    Minor changes, db connection prepared.
    Signed-off-by: zamfiralina <alina.zamfir_1997@yahoo.com>

---
+++ Backend/DBController/DBConnection.py
@@ -0,0 +1,7 @@
+import cx_Oracle
+
+class DBConnection:
+
+    def __init__(self):
+        pass
+
--- Backend/Server.py
+++ Backend/Server.py
@@ -1,15 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class testHTTPServer_RequestHandler (BaseHTTPRequestHandler):
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
     def _set_headers(self):
-        self.send_response (200)
-        self.send_header ('Content-type', 'text/html')
-        self.end_headers ( )
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
     def do_HEAD(self):
-        self._set_headers ( )
+        self._set_headers()
 
     # =================================GET==============================================#
     def do_GET(self):
@@ -19,33 +19,32 @@ class testHTTPServer_RequestHandler (BaseHTTPRequestHandler):
 
         self.path = ".../Frontend" + self.path
         try:
-            file_to_open = open (self.path[1:]).read ( )
-            self.send_response (200)
+            file_to_open = open(self.path[1:]).read( )
+            self.send_response(200)
         except:
             file_to_open = "File not found"
-            self.send_response (404)
+            self.send_response(404)
 
-        self.end_headers ( )
+        self.end_headers()
 
-        self.wfile.write (bytes (file_to_open, "utf8"))
+        self.wfile.write (bytes(file_to_open, "utf8"))
 
     #================================POST=====================================#
     def do_POST(self):
         # Doesn't do anything with posted data
-        self._set_headers ( )
+        self._set_headers( )
         #self.wfile.write ("<html><body><h1>POST!</h1></body></html>")
         
 
 
 def run():
-    print ('starting server...')
+    print('starting server...')
 
     # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
     server_address = ('127.0.0.1', 8081)
-    httpd = HTTPServer (server_address, testHTTPServer_RequestHandler)
-    print ('running server...')
-    httpd.serve_forever ( )
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
 
 
-run ( )
+run()
