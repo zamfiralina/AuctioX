f4a86bbad2c4144825e3ca66a811b829cc28aaa9
moisaanca <moisaanca@users.noreply.github.com>
2018-06-07 18:04:50 +0300

    Add files via upload

--- Database/Database.sql
+++ Database/Database.sql
@@ -63,6 +63,56 @@ CREATE TABLE AUCTIONS (USER_ID NUMBER NOT NULL ,
 
                        
 
+insert into site_users values(1,'Nume','Prenume','email@gmail.com','username','pass');
+insert into site_users values(2,'Anca','Moisa','anca@gmail.com','ancam','parola1');
+insert into site_users values(3,'Alina','Zamfir','alina@gmail.com','alinaz','hr327g');
+insert into site_users values(4,'Iulian','Andronache','iulian@gmail.com','aiuian','p32ruih');
+insert into site_users values(5,'Flori','Tanasiu','tanasiu@gmail.com','florentina','387yhu');
+insert into site_users values(6,'Rares','Dima','rdima@gmail.com','rdima','387yhu');
+insert into site_users values(7,'Matei','Tarevici','mtarevici@gmail.com','mtarevici','yhuuu');
+insert into site_users values(8,'Vasile','Popescu','vasi@gmail.com','vasipop','vasip');
+insert into site_users values(9,'Ion','Ionescu','ionescu@gmail.com','ionion','387ion');
+insert into site_users values(10,'Alex','Alexa','alex@gmail.com','alex','alehu');
+
+insert into items values(12,2,'sticla','sticla plastic suc piersici',sysdate,sysdate);
+insert into items values(13,4,'cutit','cutit spania ascutit',sysdate,sysdate);
+insert into items values(10,2,'telefon','huawei p9 lite',sysdate,sysdate);
+insert into items values(5,1,'laptop','laptop negru prafuit',sysdate,sysdate);
+insert into items values(4,1,'scaun','scaun verde scartaie',sysdate,sysdate);
+insert into items values(3,10,'cafea','cafea da energie ca sa nu mori',sysdate,sysdate);
+insert into items values(2,9,'geanta','geanta cara chestii something',sysdate,sysdate);
+insert into items values(1,8,'incarcator','something something',sysdate,sysdate);
+insert into items values(6,7,'papuci','talpa verde',sysdate,sysdate);
+insert into items values(7,5,'ochelari','ochelari prafuiti si plini de urme',sysdate,sysdate);
+
+insert into categories values(1,'electrocasnice');
+insert into categories values(2,'aparatura');
+insert into categories values(3,'mancare');
+insert into categories values(4,'obiecte');
+
+insert into item_category values(1,12,3);
+insert into item_category values(2,10,4);
+insert into item_category values(3,10,4);
+insert into item_category values(4,10,2);
+insert into item_category values(4,5,2);
+insert into item_category values(5,5,4);
+insert into item_category values(6,4,4);
+insert into item_category values(7,3,3);
+insert into item_category values(8,2,4);
+insert into item_category values(9,1,2);
+insert into item_category values(10,1,4);
+insert into item_category values(10,6,4);
+insert into item_category values(10,7,4);
 
 
+insert into auctions values(4,12,123);
+insert into auctions values(5,10,500);
+insert into auctions values(3,13,200);
+insert into auctions values(7,1,280);
+insert into auctions values(7,7,800);
+insert into auctions values(9,2,200);
+insert into auctions values(9,3,200);
+insert into auctions values(10,4,200);
+insert into auctions values(1,5,200);
+insert into auctions values(2,6,200);
 
