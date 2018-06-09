c8469676df15f8287af935ab5905c75dca557165
moisaanca <moisaanca@users.noreply.github.com>
2018-06-09 21:38:00 +0300

    Added function for register
    Added function for register

--- Frontend/functions.js
+++ Frontend/functions.js
@@ -162,3 +162,136 @@ function getLoggedInUserProfileInfo() {
 function isUserLoggedIn() {
     return getValueFromCookies("userHash").localeCompare("");
 }
+
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+}
+
+function register() {
+
+    if (document.getElementById("your_fsname").value.length <= 5) {
+        window.alert("First name is too short(min. 5 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_fsname").value) === false)
+    {
+        window.alert("Enter only alpha numeric values.");
+        return;
+    }
+
+    if (document.getElementById("your_lsname").value.length <= 5)
+    {
+        window.alert("Last name is too short(min. 5 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_lsname").value) === false)
+    {
+        window.alert("Enter only alphanumeric values.");
+        return;
+    }
+
+    if (document.getElementById("your_username").value.length <= 4)
+    {
+        window.alert("Username too short(min. 4 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_username").value) === false)
+    {
+        window.alert("Enter only alphanumeric values.");
+        return;
+    }
+
+    if (document.getElementById("your_pass").value.length <= 6)
+    {
+        window.alert("Password too short(min. 6 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_pass").value) === false)
+    {
+        window.alert("Enter only alphanumeric values.");
+        return;
+    }
+
+    var n = document.getElementById("your_pass").value.localeCompare(document.getElementById("your_pass_conf").value);
+    if (n !== 0)
+    {
+        window.alert("Password doesn't match.");
+        return;
+    }
+
+    if (document.getElementById("your_city").value.length < 4)
+    {
+        window.alert("City name too short(min. 4 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_city").value) === false)
+    {
+        window.alert("Enter only alphanumeric values.");
+        return;
+    }
+
+    if (document.getElementById("your_tel").value.length < 6)
+    {
+        window.alert("Telephone number too short(min. 6 characters).");
+        return;
+    }
+
+    if (isAlphaNumeric(document.getElementById("your_tel").value) === false)
+    {
+        window.alert("Enter only alphanumeric values.");
+        return;
+    }
+
+    if (document.getElementById("your_email").value.length < 4)
+    {
+        window.alert("Email too short(min. 5 characters).");
+        return;
+    }
+
+    if (document.getElementById("your_picture").value.length < 5)
+    {
+        window.alert("Link too short(min. 5 characters).");
+        return;
+    }
+
+
+
+
+    var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200){
+                    //document.getElementById("getText").innerHTML = this.responseText
+                    window.alert(this.responseText)
+                }
+            };
+
+            var requestText = "REGISTER" +
+                    "?" + document.getElementById("your_fsname").value +
+                    "?" + document.getElementById("your_lsname").value +
+                    "?" + document.getElementById("your_username").value +
+                    "?" + document.getElementById("your_pass").value +
+                    "?" + document.getElementById("your_country").value +
+                    "?" + document.getElementById("your_city").value +
+                    "?" + document.getElementById("your_tel").value +
+                    "?" + document.getElementById("your_email").value +
+                    "?" + document.getElementById("your_picture").value;
+
+            xhttp.open("GET", requestText, true);
+            xhttp.send();
+}
